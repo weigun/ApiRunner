822be5cd2df95b096d6bd6b570875f7a8052d829
weigun <283257958@qq.com>
2019-08-19 14:42:23 +0800

    parser

--- src/ApiRunner/business/template/paser/paser.go
+++ src/ApiRunner/business/template/paser/paser.go
@@ -13,13 +13,29 @@ func isEOF(token lexer.Token) bool {
 
 type Bucket struct {
 	Fields [][]string //二维数组来存放所有的refs
-	Vars   map[string]interface{}
-	funcs  map[string]interface{} //interface as params
+	Vars   []string
+	funcs  []map[string]interface{} //interface as params
 
 }
 
+/*
+${email}  //var
+${gen_email()}  //function
+${gen_email(4)}  //function with one args
+${gen_email(4,12)}  //function with args
+${gen_email($min,$max)}  //function with args
+${gen_email(4,$max)}  //function with mixed
+${refs.user1.email}  //function with args
+has ${getnum()} items,${num2} records
+has num items,num2 .{[(}records
+ 	//null
+*/
 func Parse(input string) (string, error) {
 	l := lexer.NewLexer(`test`, input)
+	bucketPtr := &Bucket{}
+	fieldNode := []string{} //refs.user1.email分别存放refs user email
+	funcNode := make(map[string]interface{})
+	var preTokenType lexer.TokenType
 	for {
 		_token := l.NextToken()
 		switch _token.Typ {
@@ -31,7 +47,40 @@ func Parse(input string) (string, error) {
 			// TODO
 		case lexer.TokenField:
 			//refs
+			fieldNode = append(fieldNode, _token.Val)
+		case lexer.TokenRightDelim:
+			//一个模板结束
+			//根据前一个token来决定什么逻辑
+			switch preTokenType {
+			case lexer.TokenField:
+				//应该是refs
+				fields := bucketPtr.Fields
+				fields = append(fields, fieldNode)
+				bucketPtr.Fields = fields
+				fieldNode = []string{} //reset
+			default:
+				fmt.Println(`not handle token `, _token)
+			}
+
+		case lexer.TokenVariable:
+			//vars
+			bucketPtr.Vars = append(bucketPtr.Vars, _token.Val)
+		case lexer.TokenFuncName:
+			//function
+			funcNode[_token.Val] = []string{}
+		case lexer.TokenRawParam:
+			//function params
+			if len(funcNode) != 1 {
+				panic(`more than one func in one token`)
+			}
+			for k, v := range funcNode {
+				v = append(v, _token.Val)
+				funcNode[k] = v
+				break
+			}
+			//here
 		}
+		preTokenType = _token.Typ
 
 	}
 }
